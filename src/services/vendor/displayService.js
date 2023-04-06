c1a8283266e549c541790b99d31f61334d619c9e
sarvesh <sarvesh@dev-story.com>
2023-04-06 18:48:10 +0530

    updates

--- src/services/vendor/displayService.js
+++ src/services/vendor/displayService.js
@@ -3,9 +3,17 @@ const { Vendor } = require("../../models");
 const { AuthFailedError } = require("../../utils/errors");
 
 exports.addScreen = async (vendorId, body) => {
+  let screen = [
+    {
+      name: body.name,
+      screenLocation: body.screenLocation,
+      googleLocation: body.googleLocation,
+      tags: body.tags,
+    },
+  ];
   const vendor = await Vendor.findOneAndUpdate(
     { _id: vendorId },
-    {},
+    { $addToSet: { screens: screen } },
     { new: 1, lean: 1 }
   );
   if (!vendor) {
