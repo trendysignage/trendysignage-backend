9b78a3184791f5640737cd0b518243fa79a85973
sarvesh <sarvesh@dev-story.com>
2023-04-19 15:16:03 +0530

    updates

--- src/libs/socket.js
+++ src/libs/socket.js
@@ -93,6 +93,7 @@ export const connectSocket = (server) => {
       );
     }
   }).on("connection", async (socket) => {
+    console.log(userCache, "userCachhheeeeee");
     userCache[socket.handshake.query.deviceToken].map(async (id) => {
       console.log(id, "socket idddddd");
       const vendorId = await socketService.getVendor(
