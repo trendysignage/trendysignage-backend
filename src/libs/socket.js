c45f6ccd037c9674dd0a5e1befbca576a3a28157
sarvesh <sarvesh@dev-story.com>
2023-04-20 15:55:56 +0530

    updates

--- src/libs/socket.js
+++ src/libs/socket.js
@@ -31,42 +31,6 @@ export const connectSocket = (server) => {
   });
   io.use(function (socket, next) {
     console.log("user is trying to connect");
-    if (socket.handshake.query.token) {
-      console.log("user entered");
-      jwt.verify(
-        socket.handshake.query.token,
-        config.jwt.secret,
-        async function (err, decoded) {
-          if (err || decoded.role == USER_TYPE.ADMIN) {
-            console.log(err, "errorr connneecctionnn");
-            throw new AuthFailedError(
-              ERROR_MESSAGES.AUTHENTICATION_FAILED,
-              STATUS_CODES.AUTH_FAILED
-            );
-          }
-          const token = await Token.findOne({
-            token: socket.handshake.query.token,
-          }).lean();
-
-          console.log(
-            "decoded",
-            decoded,
-            token,
-            "qwwwwwwwweerttttttttttyyyyyy"
-          );
-          socket.decoded = decoded;
-          socket.decoded.user = token.vendor;
-          let value = socket.decoded.user;
-          if (!userCache[value]) {
-            userCache[value] = [socket.id];
-          } else {
-            userCache[value].push(socket.id);
-          }
-          console.log("socketHolder", userCache);
-          return next();
-        }
-      );
-    }
     if (socket.handshake.query.deviceToken) {
       console.log("device entered");
       async function device(deviceToken) {
@@ -89,21 +53,24 @@ export const connectSocket = (server) => {
         return next();
       }
       device(socket.handshake.query.deviceToken);
+    } else {
+      throw new AuthFailedError(
+        ERROR_MESSAGES.AUTHENTICATION_FAILED,
+        STATUS_CODES.ACTION_FAILED
+      );
     }
   }).on("connection", async (socket) => {
     console.log(userCache, "userCachhheeeeee");
-    if (socket.handshake.query.deviceToken) {
-      userCache[socket.handshake.query.deviceToken].map(async (id) => {
-        console.log(id, "socket idddddd");
-        const vendorId = await socketService.getVendor(
-          socket.handshake.query?.deviceToken
-        );
-        if (vendorId) {
-          const defaultContent = await socketService.getDefault(vendorId);
-          io.to(id).emit("receiveContent", defaultContent, "emitted default");
-        }
-      });
-    }
+    userCache[socket.handshake.query.deviceToken].map(async (id) => {
+      console.log(id, "socket idddddd");
+      const vendorId = await socketService.getVendor(
+        socket.handshake.query?.deviceToken
+      );
+      if (vendorId) {
+        const defaultContent = await socketService.getDefault(vendorId);
+        io.to(id).emit("receiveContent", defaultContent, "emitted default");
+      }
+    });
     // socket.on("sendContent", async (data) => {
     //   if (!data.screenId && !data.mediaId && !data.duration) {
     //     throw new AuthFailedError(
@@ -132,15 +99,15 @@ export const connectSocket = (server) => {
     });
     socket.on("disconnect", async (data) => {
       // console.log("disconnect....", socket.id, userCache[socket.decoded.user]);
-      if (!socket.decoded) {
-        userCache[socket.handshake.query.deviceToken] = userCache[
-          socket.handshake.query.deviceToken
-        ].filter((socketId) => socketId !== socket.id);
-      } else {
-        userCache[socket.decoded.user] = userCache[socket.decoded.user].filter(
-          (socketId) => socketId !== socket.id
-        );
-      }
+      // if (!socket.decoded) {
+      //   userCache[socket.handshake.query.deviceToken] = userCache[
+      //     socket.handshake.query.deviceToken
+      //   ].filter((socketId) => socketId !== socket.id);
+      // } else {
+      userCache[socket.decoded.user] = userCache[socket.decoded.user].filter(
+        (socketId) => socketId !== socket.id
+      );
+      // }
       console.log("disconneted", userCache);
     });
   });
