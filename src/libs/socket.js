6fdcab8a0606a9d5746e0e63e94fee0ae5b6eae6
sarvesh <sarvesh@dev-story.com>
2023-04-25 11:49:33 +0530

    updates

--- src/libs/socket.js
+++ src/libs/socket.js
@@ -48,7 +48,7 @@ export const connectSocket = (server) => {
         if (!userCache[value]) {
           userCache[value] = [socket.id];
         } else {
-          userCache[value] = [socket.id];
+          userCache[value].push(socket.id);
         }
         return next();
       }
