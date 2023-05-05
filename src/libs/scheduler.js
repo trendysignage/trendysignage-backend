2e6437ea0d0b1becb90585286e7b3e5dfecc30d1
sarvesh <sarvesh@dev-story.com>
2023-05-05 11:03:59 +0530

    updates

--- src/libs/scheduler.js
+++ src/libs/scheduler.js
@@ -1,26 +1,53 @@
 import cron from "node-cron";
 import { Device, Schedule, Screen } from "../models/index.js";
 import { emit } from "../services/socketService.js";
+import { AuthFailedError } from "../utils/errors.js";
+import { STATUS_CODES } from "../config/appConstants.js";
 
-const task = async () => {
-  const screens = await Screen.find({
-    isDeleted: false,
-    schedule: { $exists: true },
-  }).lean();
-  for (const s of screens) {
-    let schedule = await Schedule.findOne({ _id: s.schedule }).lean();
-    let device = await Device.findOne({
-      _id: s.device,
+const task = async (req, res) => {
+  try {
+    const screens = await Screen.find({
       isDeleted: false,
+      schedule: { $exists: true },
     }).lean();
-
-    schedule.sequence = schedule.sequence.filter(
-      (seq) => seq.endTime > new Date() && seq.startTime < new Date()
-    );
-
-    if (schedule.sequence.length) {
-      await emit(device.deviceToken, schedule.sequence);
+    for (const s of screens) {
+      let schedule = await Schedule.findOne(
+        {
+          _id: s.schedule,
+          "sequence.dates": { $in: [new Date().toISOString().split("T")[0]] },
+          "sequence.timings": {
+            $elemMatch: {
+              startTime: { $lte: new Date() },
+              endTime: { $gte: new Date() },
+            },
+          },
+        },
+        {
+          sequence: {
+            $elemMatch: {
+              dates: { $in: [new Date().toISOString().split("T")[0]] },
+              timings: {
+                $elemMatch: {
+                  startTime: { $lte: new Date() },
+                  endTime: { $gte: new Date() },
+                },
+              },
+            },
+          },
+        }
+      );
+      let device = await Device.findOne({
+        _id: s.device,
+        isDeleted: false,
+      }).lean();
+      if (schedule.sequence) {
+        console.log("emitting.......");
+        await emit(device.deviceToken, schedule.sequence);
+      }
     }
+  } catch (err) {
+    console.log(err, "errrrr");
+    throw new AuthFailedError(err, STATUS_CODES.ACTION_FAILED);
   }
 };
 
