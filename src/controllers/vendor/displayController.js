25f590fd220aebe298d8b5d3f44b913859494d8a
sarvesh <sarvesh@dev-story.com>
2023-05-16 12:45:57 +0530

    updates

--- src/controllers/vendor/displayController.js
+++ src/controllers/vendor/displayController.js
@@ -124,5 +124,5 @@ export const mediaFile = catchAsync(async (req, res) => {
     req.token.vendor._id,
     req.query.path
   );
-  res.statusCode(STATUS_CODES.SUCCESS).sendFile(file);
+  res.sendFile(file);
 });
