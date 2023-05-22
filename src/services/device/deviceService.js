85ab08da32921af1359ab3c45a4caa88f7919331
sarvesh <sarvesh@dev-story.com>
2023-05-22 16:23:33 +0530

    updates

--- src/services/device/deviceService.js
+++ src/services/device/deviceService.js
@@ -64,7 +64,10 @@ export const addDevice1 = async (deviceToken, code) => {
     }
     device.content = [];
     device.composition = [];
-    if (screen.contentPlaying[0].type === CONTENT_TYPE.MEDIA) {
+    if (
+      screen.contentPlaying &&
+      screen.contentPlaying[0].type === CONTENT_TYPE.MEDIA
+    ) {
       device.content = screen.contentPlaying ? screen.contentPlaying : [];
     } else {
       const composition = await Composition.findById(
