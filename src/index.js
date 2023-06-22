05c543668faea48b8a7642d8761bf3d37823dd35
sarvesh <sarvesh@dev-story.com>
2023-06-22 18:54:46 +0530

    updates

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import app from "./app.js";
 import config from "./config/config.js";
 import * as socket from "./libs/socket.js";
 import CreateAdmin from "./utils/bootstrap.js";
+import cron from "./libs/scheduler.js"
 
 let server;
 mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
