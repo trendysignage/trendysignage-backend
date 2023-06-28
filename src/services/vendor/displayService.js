5017bf55144846af42a5d41fec7c203ae3c97436
sarvesh <sarvesh@dev-story.com>
2023-06-28 12:42:06 +0530

    updates

--- src/services/vendor/displayService.js
+++ src/services/vendor/displayService.js
@@ -356,13 +356,3 @@ export const mediaFile = async (filePath) => {
   }
   return file;
 };
-
-async function updt() {
-  console.log("updating screeenss");
-  await Screen.findOneAndUpdate(
-    { _id: "649bd8794aa1a7b8fd5e013b" },
-    { $set: { contentPlaying: [] } }
-  );
-}
-
-updt();
