54ca8c38df97e43f8196273fd49317fe53ad2184
sarvesh <sarvesh@dev-story.com>
2023-06-28 15:30:42 +0530

    updates

--- src/services/vendor/displayService.js
+++ src/services/vendor/displayService.js
@@ -326,11 +326,13 @@ export const publish = async (vendorId, body, timezone) => {
 
   for (const id of body.screenIds) {
     let screen = await Screen.findOne({ _id: id, isDeleted: false });
-    const index = screen.contentPlaying.findIndex(
-      (item) => item.type === CONTENT_TYPE.MEDIA
-    );
-    if (index !== -1) {
-      screen.contentPlaying.splice(index, 1);
+    if (body.type === CONTENT_TYPE.MEDIA) {
+      const index = screen.contentPlaying.findIndex(
+        (item) => item.type === CONTENT_TYPE.MEDIA
+      );
+      if (index !== -1) {
+        screen.contentPlaying.splice(index, 1);
+      }
     }
     screen.contentPlaying.push(contentPlaying);
     screen = await screen.save();
