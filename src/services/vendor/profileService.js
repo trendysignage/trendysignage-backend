321a4947c05b2c4e240eae1e1236d4536411cd85
sarvesh <sarvesh@dev-story.com>
2023-07-05 12:22:02 +0530

    updates

--- src/services/vendor/profileService.js
+++ src/services/vendor/profileService.js
@@ -109,9 +109,23 @@ export const uptimeReport = async (vendorId, query) => {
         $elemMatch: { day: { $gte: query.startDate, $lte: query.endDate } },
       },
     },
-    { screen: 1, "uptimeReport.$": 1 },
+    {
+      name: 1,
+      "uptimeReport.$": 1,
+      screenLocation: 1,
+      googleLocation: 1,
+      tags: 1,
+      defaultComposition: 1,
+      createdAt: 1,
+      updatedAt: 1,
+      groups: 1,
+      isConnected: 1,
+      schedule: 1,
+      contentPlaying: 1,
+      device: 1,
+    },
     paginationOptions(query.page, query.limit)
-  );
+  ).populate([{ path: "device" }, { path: "schedule" }]);
 
   return reports;
 };
