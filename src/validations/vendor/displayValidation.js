f9d103c49e0b6f3f344460943285a94f61830d80
Sarvesh <sarvesh@dev-story.com>
2023-07-12 12:50:42 +0530

    updates

--- src/validations/vendor/displayValidation.js
+++ src/validations/vendor/displayValidation.js
@@ -1,5 +1,6 @@
 import Joi from "joi";
 import { JOI } from "../../config/appConstants.js";
+import { objectId } from "../custom.validation.js";
 
 export const deviceCode = {
   body: Joi.object().keys({
