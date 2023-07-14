7c4c986b5227c6f15b593fb05a38c553e78b0c4f
Sarvesh <sarvesh@dev-story.com>
2023-07-14 16:29:57 +0530

    updates

--- src/services/vendor/profileService.js
+++ src/services/vendor/profileService.js
@@ -135,8 +135,8 @@ export const uptimeReport = async (vendorId, query) => {
     paginationOptions(query.page, query.limit)
   );
 
-  reports.map((report) => {
-    report.uptimeReport = report.uptimeReport?.filter(
+  reports?.map((report) => {
+    report.uptimeReport = report?.uptimeReport?.filter(
       (i) => i.day >= query.startDate && i.day <= query.endDate
     );
   });
