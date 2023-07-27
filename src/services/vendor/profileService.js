de8c2803d974c43dbf31d1cc5c031d58173bdd5e
sarvesh <sarvesh@dev-story.com>
2023-07-27 11:57:28 +0530

    updates

--- src/services/vendor/profileService.js
+++ src/services/vendor/profileService.js
@@ -163,7 +163,9 @@ export const mediaReport = async (vendorId, query) => {
 
   const reducedReport = vendor?.mediaReport?.reduce((acc, curr) => {
     const { media, duration, loop } = curr;
-    const mediaObject = vendor.media.find((m) => console.log(m._id === media));
+    const mediaObject = vendor.media.find((m) =>
+      console.log(JSON.stringify(m._id) === JSON.stringify(media))
+    );
     console.log(mediaObject);
     if (acc[mediaObject]) {
       acc[mediaObject].loop += Number(loop);
