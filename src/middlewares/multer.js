4c34d3faac19339c85381ac716ed04b8ce94b3b4
sarvesh <sarvesh@dev-story.com>
2023-08-16 16:07:59 +0530

    updates

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -4,7 +4,7 @@ import path from "path";
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    console.log(req, "baseUrl multer", file);
+    console.log(req.file, "baseUrl multer");
     const vendorId = req.token.vendor._id;
     const mediaType = file.mimetype.split("/")[0];
     const dest = `public/${vendorId}/${mediaType}`;
