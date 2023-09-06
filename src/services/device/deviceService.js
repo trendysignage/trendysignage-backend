c080cd62a81a58a98004dc9806acd2f293b2ce52
sarvesh <sarvesh@dev-story.com>
2023-09-06 11:18:12 +0530

    updates

--- src/services/device/deviceService.js
+++ src/services/device/deviceService.js
@@ -73,13 +73,14 @@ export const addDevice = async (deviceToken, code, timezone) => {
       device.content = screen?.contentPlaying ?? [];
     }
 
-    console.log(JSON.stringify(device.content), "cjkdghvfeh");
-
     for (const content of device?.content) {
       for (const zone of content?.media?.zones) {
         for (const s of zone?.content) {
+          console.log(JSON.stringify(s), "first data");
           if (s?.type === "rss-apps") {
             s.data = JSON.parse(s?.data);
+            console.log(JSON.stringify(s), "cjkdghvfeh");
+
             if (s.data.urlLink) {
               s.data.urlLink = await parser.parseURL(s?.data?.urlLink);
             }
