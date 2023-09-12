2940298a99e34c09ba5b37e36669ad19fc59a347
sarvesh <sarvesh@dev-story.com>
2023-09-12 15:39:54 +0530

    updates

--- src/services/admin/vendorService.js
+++ src/services/admin/vendorService.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcryptjs";
 import { ERROR_MESSAGES, STATUS_CODES } from "../../config/appConstants.js";
-import { Vendor } from "../../models/index.js";
+import { Composition, Vendor } from "../../models/index.js";
 import { AuthFailedError } from "../../utils/errors.js";
 import {
   generateId,
