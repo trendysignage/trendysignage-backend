fde07c0ef92a522e049f3e6338a5e90a1f529aa8
sarvesh <sarvesh@dev-story.com>
2023-09-13 17:42:54 +0530

    updates

--- src/utils/bootstrap.js
+++ src/utils/bootstrap.js
@@ -198,35 +198,34 @@ const createLayouts = async () => {
 };
 
 const defaultComposition = async () => {
-  // const layout = await Layout.findOne({
-  //   title: "Single Zone Landscape",
-  // }).lean();
+  const layout = await Layout.findOne({
+    title: "Single Zone Landscape",
+  }).lean();
 
-  // const zones = [
-  //   {
-  //     name: "Zone1",
-  //     zoneId: layout.zones[0]._id,
-  //     content: [
-  //       {
-  //         url: config.defaultComposition,
-  //         type: "image",
-  //         maintainAspectRatio: false,
-  //         fitToScreen: true,
-  //         crop: false,
-  //         duration: 10,
-  //       },
-  //     ],
-  //   },
-  // ];
+  const zones = [
+    {
+      name: "Zone1",
+      zoneId: layout.zones[0]._id,
+      content: [
+        {
+          url: config.defaultComposition,
+          type: "image",
+          maintainAspectRatio: false,
+          fitToScreen: true,
+          crop: false,
+          duration: 10,
+        },
+      ],
+    },
+  ];
 
-  // const composition = await Composition.create({
-  //   name: "Default Composition",
-  //   layout: layout._id,
-  //   zones,
-  //   duration: 10,
-  //   type: "composition",
-  // });
-  await Composition.deleteMany({ name: "Default Composition" });
+  const composition = await Composition.create({
+    name: "Default Composition",
+    layout: layout._id,
+    zones,
+    duration: 10,
+    type: "composition",
+  });
 };
 
 export default Run;
