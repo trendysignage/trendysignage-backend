02236b055097fbbdee7a3784fbe9cb2713d0d054
sarvesh <sarvesh@dev-story.com>
2023-09-13 17:38:29 +0530

    updates

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -8,7 +8,7 @@ const storage = multer.diskStorage({
     const vendorId = req.token.vendor._id;
     const mediaType = file.mimetype.split("/")[0];
     // const dest = `public/${vendorId}/${mediaType}`;
-    const dest = `default/`;
+    const dest = `public/default/`;
 
     // create the destination folder if it doesn't exist
     if (!fs.existsSync(dest)) {
