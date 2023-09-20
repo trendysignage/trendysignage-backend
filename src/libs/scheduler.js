0ae3a6c801fb5a63b790db64bb4961ce24b04577
sarvesh <sarvesh@dev-story.com>
2023-09-20 11:48:17 +0530

    updates

--- src/libs/scheduler.js
+++ src/libs/scheduler.js
@@ -63,6 +63,8 @@ const task = async (req, res) => {
         );
       }
 
+console.log(JSON.stringify(schedule), "rgkjnjn")
+
       let device = await Device.findOne({
         _id: s.device,
         isDeleted: false,
