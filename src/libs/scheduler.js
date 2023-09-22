4a8774ad95d1f370bbe32a025a3c2e69a67ff3c4
sarvesh <sarvesh@dev-story.com>
2023-09-22 19:06:48 +0530

    updates

--- src/libs/scheduler.js
+++ src/libs/scheduler.js
@@ -27,25 +27,19 @@ const task = async (req, res) => {
     const currentTime = moment.tz(new Date(), timezone);
 
     const currentDate = moment.tz(timezone).format("YYYY-MM-DD");
-    
-    console.log(await Schedule.findOne({_id: "650d43909a9fe2eff3a4cb30"}))
 
     for (const s of screens) {
-      const current = moment().format("YYYY-MM-DD");
-
-
-      let schedule = await Schedule.findOne({
-        _id: s.schedule,
-        "sequence.dates": {
-          $in: [new Date(current)],
+      let schedule = await Schedule.findOne(
+        {
+          _id: s.schedule,
+          "sequence.dates": {
+            $in: [new Date(current)],
+          },
         },
-      })
+        { "sequence.$": 1 }
+      )
         .populate({ path: "sequence.timings.composition" })
         .lean();
-
-      if (schedule?._id == "650a84ddb85b1787de912b7d") {
-        console.log(JSON.stringify(schedule), "bjh");
-      }
 
       if (schedule) {
         schedule.sequence[0].timings = schedule.sequence[0].timings.filter(
