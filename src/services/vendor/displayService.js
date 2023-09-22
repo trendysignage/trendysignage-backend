0306eb9810e6118b75ce78b656bc3cb3c56024d8
sarvesh <sarvesh@dev-story.com>
2023-09-22 13:30:19 +0530

    updates

--- src/services/vendor/displayService.js
+++ src/services/vendor/displayService.js
@@ -545,6 +545,7 @@ export const mediaDetail = async (_id, mediaId) => {
 export const assignGroup = async (_id, screenId, groupIds) => {
   const vendor = await Vendor.findById(_id, { groups: 1 }).lean();
 
+  console.log(groupIds, groupIds.length);
   if (groupIds.length > 0)
     groupIds?.forEach((id) => {
       const group = vendor?.groups?.find(
