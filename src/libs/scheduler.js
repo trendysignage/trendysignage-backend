26851c9adf48ad574308f53ea23c34366ba9b573
sarvesh <sarvesh@dev-story.com>
2023-09-22 18:52:01 +0530

    updates

--- src/libs/scheduler.js
+++ src/libs/scheduler.js
@@ -38,7 +38,7 @@ const task = async (req, res) => {
         .populate({ path: "sequence.timings.composition" })
         .lean();
 
-      if (JSON.stringify(schedule?._id) == "650a84ddb85b1787de912b7d") {
+      if (schedule.JSON.stringify(_id) == "650a84ddb85b1787de912b7d") {
         console.log(JSON.stringify(schedule), "bjh");
       }
 
