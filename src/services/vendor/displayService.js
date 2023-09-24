f31321d1f172b0393da5ebe1646c1a75f62acaf7
Sarvesh <sarvesh@dev-story.com>
2023-09-24 12:38:55 +0530

    updates

--- src/services/vendor/displayService.js
+++ src/services/vendor/displayService.js
@@ -296,7 +296,7 @@ export const getMedia = async (query, vendorId) => {
 
     query.tags.map((tag) => {
       vendor.media = vendor.media.filter(
-        (m) => console.log(JSON.stringify(m.tags).includes(tag), tag)
+        (m) => console.log(JSON.stringify(m.tags), tag)
         // JSON.stringify(m.tags).includes(tag)
       );
     });
