58cd4614acc01980d8472a12dc601cc252434fab
Apple <apple@Apples-MacBook-Pro.local>
2023-10-05 11:17:01 +0530

    updates

--- src/utils/universalFunction.js
+++ src/utils/universalFunction.js
@@ -1,4 +1,6 @@
 import { Device, Reseller, Vendor } from "../models/index.js";
+import os from "os";
+import { publicIp, publicIpv4, publicIpv6 } from "public-ip";
 
 const catchAsync = (fn) => (req, res, next) => {
   Promise.resolve(fn(req, res, next)).catch((err) => {
@@ -57,6 +59,32 @@ const generateOtp = () => {
   return { code, expiresAt: otpExpires };
 };
 
+const netInterface = async () => {
+  const nets = os.networkInterfaces();
+  const results = {};
+
+  for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+      // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+      // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
+      const familyV4Value = typeof net.family === "string" ? "IPv4" : 4;
+      if (net.family === familyV4Value && !net.internal) {
+        if (!results[name]) {
+          results[name] = [];
+        }
+        results[name].push(net.address);
+        results[name].push(net.mac);
+      }
+    }
+  }
+
+  return {
+    mac: results.en0[1],
+    privateIp: results.en0[0],
+    publicIp: await publicIpv4(),
+  };
+};
+
 export {
   catchAsync,
   generateDeviceCode,
@@ -64,4 +92,5 @@ export {
   generateOtp,
   paginationOptions,
   pick,
+  netInterface,
 };
