ae8b13374d33528012b9040ea2864d8121836784
sarvesh <sarvesh@dev-story.com>
2023-10-17 14:49:14 +0530

    multer try catch

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -4,23 +4,27 @@ import path from "path";
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    console.log(file, "baseUrl multer");
-    const vendorId = "req.token.vendor._id";
-    const mediaType = file.mimetype.split("/")[0];
-    const dest = `public/${vendorId}/${mediaType}`;
-    // const dest = `public/default/`;
+    try {
+      console.log(file, "baseUrl multer");
+      const vendorId = "req.token.vendor._id";
+      const mediaType = file.mimetype.split("/")[0];
+      const dest = `public/${vendorId}/${mediaType}`;
+      // const dest = `public/default/`;
 
-    // create the destination folder if it doesn't exist
-    if (!fs.existsSync(dest)) {
-      // create the destination folder (and any parent folders) if it doesn't exist
-      fs.mkdir(dest, { recursive: true }, (err) => {
-        if (err) {
-          console.error("Error creating destination folder:", err);
-        }
+      // create the destination folder if it doesn't exist
+      if (!fs.existsSync(dest)) {
+        // create the destination folder (and any parent folders) if it doesn't exist
+        fs.mkdir(dest, { recursive: true }, (err) => {
+          if (err) {
+            console.error("Error creating destination folder:", err);
+          }
+          cb(null, dest);
+        });
+      } else {
         cb(null, dest);
-      });
-    } else {
-      cb(null, dest);
+      }
+    } catch (err) {
+      console.log(err, "error uploading in multer =========");
     }
   },
   filename: (req, file, cb) => {
