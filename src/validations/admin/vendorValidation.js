632da9d498477efd249c100065335daec9499101
sarvesh <sarvesh@dev-story.com>
2023-11-03 15:49:34 +0530

    updates

--- src/validations/admin/vendorValidation.js
+++ src/validations/admin/vendorValidation.js
@@ -1,5 +1,6 @@
 import Joi from "joi";
 import { JOI } from "../../config/appConstants.js";
+import { objectId } from "../custom.validation.js";
 
 export const getVendor = {
   query: Joi.object().keys({
