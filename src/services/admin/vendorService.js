4ef97ab385f9efa02ce087d9fbb6ac864c41d8a4
sarvesh <sarvesh@dev-story.com>
2023-11-29 16:07:42 +0530

    updates

--- src/services/admin/vendorService.js
+++ src/services/admin/vendorService.js
@@ -179,13 +179,3 @@ export const list = async (_id, query) => {
 
   return { vendors, count };
 };
-
-async function cc() {
-  await Vendor.updateOne(
-    { email: "demo@example.com" },
-    { $set: { totalScreens: 10 } }
-  ).lean();
-}
-
-
-cc()
