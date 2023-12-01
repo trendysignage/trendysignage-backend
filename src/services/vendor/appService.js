fec53aa77d6ec364b3a479a62b2f75e4e9e20249
sarvesh <sarvesh@dev-story.com>
2023-12-01 18:23:10 +0530

    updates

--- src/services/vendor/appService.js
+++ src/services/vendor/appService.js
@@ -4,59 +4,66 @@ import { Vendor } from "../../models/index.js";
 import { AuthFailedError } from "../../utils/errors.js";
 
 export const createApp = async (vendor, body) => {
-  const parser = new Parser();
+  try {
+    const parser = new Parser();
 
-  const subvendor = await Vendor.findById(vendor).lean();
-  if (
-    subvendor.role !== "ADMIN" &&
-    !subvendor.roles[subvendor.role]["APPS"].add
-  ) {
-    throw new AuthFailedError(
-      ERROR_MESSAGES.PERMISSION_DENIED,
-      STATUS_CODES.FORBIDDEN
+    const subvendor = await Vendor.findById(vendor).lean();
+    if (
+      subvendor.role !== "ADMIN" &&
+      !subvendor.roles[subvendor.role]["APPS"].add
+    ) {
+      throw new AuthFailedError(
+        ERROR_MESSAGES.PERMISSION_DENIED,
+        STATUS_CODES.FORBIDDEN
+      );
+    }
+
+    const media = {
+      title: body.name,
+      type: body.type,
+      appData: body.data,
+      createdBy: vendor,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    if (subvendor.vendor) media.createdBy = subvendor.vendor;
+
+    if (body.type === "youtube-apps") {
+      media.duration = 0;
+    }
+
+    const app = await Vendor.findOneAndUpdate(
+      { _id: media.createdBy },
+      { $addToSet: { media } },
+      { new: 1, lean: 1 }
     );
-  }
 
-  const media = {
-    title: body.name,
-    type: body.type,
-    appData: body.data,
-    createdBy: vendor,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  };
+    if (!app) {
+      throw new AuthFailedError(
+        ERROR_MESSAGES.VENDOR_NOT_FOUND,
+        STATUS_CODES.ACTION_FAILED
+      );
+    }
 
-  if (subvendor.vendor) media.createdBy = subvendor.vendor;
+    if (body.type === "rss-apps") {
+      app.media[app.media.length - 1].appData = JSON.parse(
+        app.media[app.media.length - 1]?.appData
+      );
+      if (app.media[app.media.length - 1].appData.urlLink) {
+        app.media[app.media.length - 1].appData.urlLink = await parser.parseURL(
+          app.media[app.media.length - 1]?.appData?.urlLink
+        );
+      }
+    }
 
-  if (body.type === "youtube-apps") {
-    media.duration = 0;
-  }
-
-  const app = await Vendor.findOneAndUpdate(
-    { _id: media.createdBy },
-    { $addToSet: { media } },
-    { new: 1, lean: 1 }
-  );
-
-  if (!app) {
+    return app.media[app.media.length - 1];
+  } catch (err) {
     throw new AuthFailedError(
-      ERROR_MESSAGES.VENDOR_NOT_FOUND,
+      ERROR_MESSAGES.RSS_APP_FAILED,
       STATUS_CODES.ACTION_FAILED
     );
   }
-
-  if (body.type === "rss-apps") {
-    app.media[app.media.length - 1].appData = JSON.parse(
-      app.media[app.media.length - 1]?.appData
-    );
-    if (app.media[app.media.length - 1].appData.urlLink) {
-      app.media[app.media.length - 1].appData.urlLink = await parser.parseURL(
-        app.media[app.media.length - 1]?.appData?.urlLink
-      );
-    }
-  }
-
-  return app.media[app.media.length - 1];
 };
 
 export const editApp = async (_id, body) => {
