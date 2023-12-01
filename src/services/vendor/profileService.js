898122a8ec7646b4e277ad5cd30ee4437c9cb7a4
MAC <mac@MACs-MacBook-Pro.local>
2023-12-02 00:21:19 +0530

    updates

--- src/services/vendor/profileService.js
+++ src/services/vendor/profileService.js
@@ -243,8 +243,9 @@ export const mediaReport = async (vendorId, query) => {
     mediaReport: 1,
   }).lean();
 
+  console.log(vendor, "vghjbnkml");
+
   if (query.startDate && query.endDate) {
-    console.log(vendor.mediaReport, "ugyihjlk;")
     vendor.mediaReport = vendor?.mediaReport?.filter(
       (report) => report.day >= query.startDate && report.day <= query.endDate
     );
